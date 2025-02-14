458c7ca2b1517e10d5c6c732d0356ddcf6996c90
NiranjanKumar003 <niranjankumarofficial003@gmail.com>
2025-02-15 01:31:57 +0530

    added the subscription and user model fro the project

--- models/subscription.model.js
+++ models/subscription.model.js
@@ -1,17 +1,76 @@
 import mongoose from "mongoose";
 
-const subscriptionSchema = new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true,'Subscription Name is required '],
-        trim:true,
-        minLength:2,
-        maxLength:100,
+const subscriptionSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Subscription Name is required "],
+      trim: true,
+      minLength: 2,
+      maxLength: 100,
     },
-    price:{
-        type:Number,
-        required:[true,'Subscription price is required '],
-        min:[0,'Price must be greater than 0'],
+    price: {
+      type: Number,
+      required: [true, "Subscription price is required "],
+      min: [0, "Price must be greater than 0"],
     },
-    
-}, { timestamps: true });
+    currency: {
+      type: String,
+      enum: ["USD", "EUR", "GBP", "INR"],
+      default: "INR",
+    },
+    frequency: {
+      type: String,
+      enum: ["daily", "weekly", "monthly", "yearly"],
+    },
+    category: {
+      type: String,
+      enum: [
+        "streaming",
+        "music",
+        "cloud",
+        "delivery",
+        "gaming",
+        "news",
+        "other",
+      ],
+      required: true,
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ["active", "cancelled", "expired"],
+      default: "active",
+    },
+    startDate: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: (value) => value <= new Date(),
+        message: "Start date must be in the past",
+      },
+    },
+    renewalDate: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: function(value) {
+             return value > this.startDate;
+            },
+        message: "renewal date must be in the past",
+      }
+    },
+
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index:true,
+    },
+  },
+  { timestamps: true }
+);
