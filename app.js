24b181e4e9b2d734f313843a3d7003413a230db5
NiranjanKumar003 <niranjankumarofficial003@gmail.com>
2025-02-15 01:11:22 +0530

    added the models

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 
 import subscriptionRouter from "./routes/subscription.routes.js";
-import connectToDatabase from "database/mongodb.js";
+import connectToDatabase from "./database/mongodb.js";
 
 const app = express();
 
