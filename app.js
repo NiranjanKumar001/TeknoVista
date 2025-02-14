0a096a7015fafc84549be63068fadfa1a4a470cb
NiranjanKumar003 <niranjankumarofficial003@gmail.com>
2025-02-15 02:03:10 +0530

    fixes

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cookieParser());
+app.use(cookieParser()); // this will parse the cookies from the request headers and make it available in the req.cookies object
 
 app.use('/api/v1/auth',authRouter);
 app.use('/api/v1/users',userRouter);
