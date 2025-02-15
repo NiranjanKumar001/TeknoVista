cfd66272eebaf615a1aac3d7f67b2be9c6388800
NiranjanKumar003 <niranjankumarofficial003@gmail.com>
2025-02-15 13:04:27 +0530

    added signin logic

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { JWT_EXPIRE_IN, JWT_SECRET } from '../config/env.js';
+import { JWT_EXPIRES_IN, JWT_SECRET } from '../config/env.js';
 import User from '../models/user.model.js';
 
 //what is a req body:-> req.body is an object containing data that comes from the client(post request)
@@ -22,7 +22,7 @@ export const signUp = async (req, res ,next) => {
 
     try{
         //create user
-        const {name,email,password}=req.body;
+        const {name ,email ,password }=req.body;
 
         //check if user already exists
         const existingUser=await User.findOne({email});
@@ -41,7 +41,7 @@ export const signUp = async (req, res ,next) => {
 
         const newUsers =await User.create([{name,email,password:hashedPassword}],{session});
 
-        const token =jwt.sign({userId:newUsers[0]._id },JWT_SECRET,{expiresIn:JWT_EXPIRE_IN});
+        const token =jwt.sign({userId:newUsers[0]._id },JWT_SECRET,{expiresIn:JWT_EXPIRES_IN});
 
         await session.commitTransaction();
 
@@ -64,9 +64,46 @@ export const signUp = async (req, res ,next) => {
 }
 export const signIn = async (req, res ,next) => {
     //signin logic
+    try {
+        const {email,password}=req.body;
+        const user= await User.findOne({email});
 
+        if(!user){
+            const error =new Error('User not found');
+            error.statusCode=404;
+            //404->not found
+            throw error;
+        }
+
+        const isPasswordValid =await bcrypt.compare(password,user.password);
+
+
+        if(!isPasswordValid){
+            const error =new Error('Invalid password');
+            error.statusCode=401;
+            //401->unauthorized
+            throw error;
+        }
+
+        const token =jwt.sign({userId:user._id },JWT_SECRET,{expiresIn:JWT_EXPIRES_IN});
+
+
+        res.status(200).json({
+            success:true,
+            message:'User signed in successfully',
+            data:{
+                token,
+                user,
+            }
+        });
+
+        
+
+    } catch (error) {
+        next(error);
+    }
 }
-export const signOut = async (req, res ,next) => {
+export const signOut = async () => {
     //signout logic
 
 }
