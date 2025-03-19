26cd00cd10b7f1a0eb1bb1b82f5d334a201438c1
NiranjanKumar003 <niranjankumarofficial003@gmail.com>
2025-03-19 18:47:11 +0530

    fixes

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -2,6 +2,7 @@ import Subscription from '../models/subscription.model.js'
 import { workflowClient } from '../config/upstash.js'
 import { SERVER_URL } from '../config/env.js'
 
+
 export const createSubscription = async (req, res, next) => {
   try {
     const subscription = await Subscription.create({
